5b0ff82b93ace9d5a267cb37222378bd0df06f0d
MarineDos <marinedos@gmail.com>
2014-11-04 13:50:55 +0100

    Create account and log in with this account To Do : redirect to home page passing session's information

--- app/src/main/java/simplycook/marinedos/com/simplycook/ConnectFragment.java
+++ app/src/main/java/simplycook/marinedos/com/simplycook/ConnectFragment.java
@@ -1,5 +1,8 @@
 package simplycook.marinedos.com.simplycook;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,7 +10,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -39,6 +45,7 @@ public class ConnectFragment extends Fragment{
 
     private static final String TAG = "ConnectFragment";
     private UiLifecycleHelper uiHelper;
+    private Button login_btn, new_account_btn;
     private final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
     private final Session.StatusCallback callback = new Session.StatusCallback() {
         @Override
@@ -49,11 +56,79 @@ public class ConnectFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.connect, container, false);
+        final View view = inflater.inflate(R.layout.connect, container, false);
 
         authButton = (LoginButton) view.findViewById(R.id.authButton);
         authButton.setFragment(this);
         authButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+
+        login_btn = (Button) view.findViewById(R.id.btn_login);
+        login_btn.setOnClickListener(new View.OnClickListener(){
+                                         @Override
+                                         public void onClick(final View v) {
+                                             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                             LayoutInflater inflater = getActivity().getLayoutInflater();
+
+                                             final View popupView = inflater.inflate(R.layout.login_dialog, null);
+                                             builder.setView(popupView)
+                                                     .setMessage(R.string.messageLoginPopup)
+                                                     .setTitle(R.string.titleLoginPopup)
+                                                     .setPositiveButton(R.string.positiveLoginPopup, new DialogInterface.OnClickListener() {
+                                                         public void onClick(DialogInterface dialog, int id) {
+                                                             // Log in the user
+                                                             View currentView = getView();
+
+                                                             EditText email_input = (EditText) popupView.findViewById(R.id.identifiant);
+                                                             String email = email_input.getText().toString();
+
+                                                             EditText password_input = (EditText) popupView.findViewById(R.id.password);
+                                                             String password = password_input.getText().toString();
+                                                             ref.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+                                                                 @Override
+                                                                 public void onAuthenticated(AuthData authData) {
+                                                                     // TO DO : change activity
+                                                                 }
+
+                                                                 @Override
+                                                                 public void onAuthenticationError(FirebaseError firebaseError) {
+                                                                     Context context = getActivity();
+                                                                     System.out.println("Firebase error : " + firebaseError.getCode());
+                                                                     int codeError = firebaseError.getCode();
+                                                                     String message = "";
+                                                                     switch(codeError){
+                                                                         case -16:
+                                                                             message = context.getString(R.string.errorMessage_incorrectPassword);
+                                                                     }
+                                                                     Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+                                                                     toast.show();
+
+                                                                 }
+                                                             });
+                                                         }
+                                                     })
+                                                     .setNegativeButton(R.string.negativeLoginPopup, new DialogInterface.OnClickListener() {
+                                                         public void onClick(DialogInterface dialog, int id) {
+                                                             // User cancelled the dialog
+                                                         }
+                                                     });
+                                             AlertDialog dialog = builder.create();
+                                             dialog.show();
+
+
+                                         }
+                                     }
+        );
+
+        new_account_btn = (Button) view.findViewById(R.id.btn_create_account);
+        new_account_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Change activity to create an account
+                Intent intent = new Intent(getActivity(), CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         return view;
     }
@@ -65,7 +140,7 @@ public class ConnectFragment extends Fragment{
         uiHelper.onCreate(savedInstanceState);
     }
 
-     // Facebook login in / out
+    // Facebook login in / out
     private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
 
         if (state.isOpened()) {
@@ -93,36 +168,27 @@ public class ConnectFragment extends Fragment{
                                         .startAt(id)
                                         .endAt(id)
                                         .addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(DataSnapshot snapshot) {
-                                        System.out.println(snapshot.getValue());
+                                            @Override
+                                            public void onDataChange(DataSnapshot snapshot) {
 
-                                        boolean exists = (snapshot.getValue() != null);
-                                        if(!exists){
-                                            System.out.println("Unexisting user : " + id);
-                                            // Create user in Firebase
-                                            System.out.println("1st : " + user.getFirstName());
-                                            System.out.println("last : " + user.getLastName());
-                                            System.out.println("last : " + user.getProperty("email"));
+                                                boolean exists = (snapshot.getValue() != null);
+                                                if(!exists){
+                                                    Map<String, String> newUser = new HashMap<String, String>();
+                                                    newUser.put("firstName", user.getFirstName());
+                                                    newUser.put("lastName", user.getLastName());
+                                                    newUser.put("email", user.getProperty("email").toString());
 
-                                            Map<String, String> newUser = new HashMap<String, String>();
-                                            newUser.put("firstName", user.getFirstName());
-                                            newUser.put("lastName", user.getLastName());
-                                            newUser.put("email", user.getProperty("email").toString());
+                                                    Firebase newRef = ref.child("/users/").push();
+                                                    newRef.setValue(newUser, id);
+                                                }else{
+                                                    System.out.println("User already exists : " + id);
+                                                }
+                                            }
 
-                                            Firebase newRef = ref.child("/users/").push();
-                                            newRef.setValue(newUser, id);
-
-
-                                        }else{
-                                            System.out.println("User already exists : " + id);
-                                        }
-                                    }
-
-                                    @Override
-                                    public void onCancelled(FirebaseError firebaseError) {
-                                    }
-                                });
+                                            @Override
+                                            public void onCancelled(FirebaseError firebaseError) {
+                                            }
+                                        });
                             }
 
                             @Override
---
+++ app/src/main/java/simplycook/marinedos/com/simplycook/CreateAccountFragment.java
@@ -0,0 +1,105 @@
+package simplycook.marinedos.com.simplycook;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Marine on 03/11/2014.
+ */
+public class CreateAccountFragment extends Fragment{
+    private Button createAccount_btn;
+    private TextView firstName_input, lastName_input, email_input, password_input;
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.create_account_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final View rootView = getView();
+        createAccount_btn = (Button) rootView.findViewById(R.id.btn_create_new_account);
+        createAccount_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean goOn = true;
+                // check if all input are full
+                firstName_input = (TextView) rootView.findViewById(R.id.firstName);
+                if(firstName_input.getText().toString().trim().equals("")){
+                    firstName_input.setError( "First name is required!" );
+                    goOn = false;
+                }
+
+                lastName_input = (TextView) rootView.findViewById(R.id.lastName);
+                if(lastName_input.getText().toString().trim().equals("")){
+                    lastName_input.setError( "Last name is required!" );
+                    goOn = false;
+                }
+
+                email_input = (TextView) rootView.findViewById(R.id.email);
+                if(email_input.getText().toString().trim().equals("")){
+                    email_input.setError( "Email is required!" );
+                    goOn = false;
+                }
+
+                password_input = (TextView) rootView.findViewById(R.id.password);
+                if(password_input.getText().toString().trim().equals("")){
+                    password_input.setError( "Password is required!" );
+                    goOn = false;
+                }
+
+                if(goOn){
+                    final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
+                    String email = email_input.getText().toString();
+                    String password = password_input.getText().toString();
+                    ref.createUser(email, password, new Firebase.ResultHandler() {
+                        @Override
+                        public void onSuccess() {
+                            // Create user in database
+                            Map<String, String> newUser = new HashMap<String, String>();
+                            newUser.put("firstName", firstName_input.getText().toString());
+                            newUser.put("lastName", lastName_input.getText().toString());
+                            newUser.put("email", email_input.getText().toString());
+
+                            Firebase newRef = ref.child("/users/").push();
+                            newRef.setValue(newUser, email_input.getText().toString());
+                        }
+
+                        @Override
+                        public void onError(FirebaseError firebaseError) {
+                            System.out.println("Error code : " + firebaseError.getCode());
+                            Context context = getActivity();
+                            String message = "";
+                            switch(firebaseError.getCode()){
+                                case -18:
+                                    message = context.getString(R.string.errorMessage_emailAlreadyUsed);
+                            }
+                            Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+                            toast.show();
+
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+}
