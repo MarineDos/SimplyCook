65385e4f9228604a5c012e81b7fc8e85574bf519
MarineDos <marinedos@gmail.com>
2014-11-11 12:32:34 +0100

    Create account task asynchrounously

--- app/src/main/java/simplycook/marinedos/com/simplycook/CreateAccountFragment.java
+++ app/src/main/java/simplycook/marinedos/com/simplycook/CreateAccountFragment.java
@@ -1,6 +1,10 @@
 package simplycook.marinedos.com.simplycook;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,6 +27,7 @@ import java.util.Map;
 public class CreateAccountFragment extends Fragment{
     private Button createAccount_btn;
     private TextView firstName_input, lastName_input, email_input, password_input;
+    private CreateAccountTask mCreateAccountTask;
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -35,9 +40,9 @@ public class CreateAccountFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        final View rootView = getView();
         final Context context = getActivity();
 
-        final View rootView = getView();
         createAccount_btn = (Button) rootView.findViewById(R.id.btn_create_new_account);
         createAccount_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,39 +73,77 @@ public class CreateAccountFragment extends Fragment{
                     goOn = false;
                 }
 
-                if(goOn){
-                    final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
-                    String email = email_input.getText().toString();
-                    String password = password_input.getText().toString();
-                    ref.createUser(email, password, new Firebase.ResultHandler() {
-                        @Override
-                        public void onSuccess() {
-                            // Create user in database
-                            Map<String, String> newUser = new HashMap<String, String>();
-                            newUser.put("firstName", firstName_input.getText().toString());
-                            newUser.put("lastName", lastName_input.getText().toString());
-                            newUser.put("email", email_input.getText().toString());
-
-                            Firebase newRef = ref.child("/users/").push();
-                            newRef.setValue(newUser, email_input.getText().toString());
-                        }
-
-                        @Override
-                        public void onError(FirebaseError firebaseError) {
-                            System.out.println("Error code : " + firebaseError.getCode());
-                            String message = "";
-                            switch(firebaseError.getCode()){
-                                case -18:
-                                    message = context.getString(R.string.errorMessage_emailAlreadyUsed);
-                            }
-                            Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-                            toast.show();
-
-                        }
-                    });
+                if(goOn) {
+                    mCreateAccountTask = new CreateAccountTask();
+                    mCreateAccountTask.execute();
                 }
 
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        if(mCreateAccountTask != null){
+            mCreateAccountTask.cancel(true);
+        }
+        super.onDestroy();
+    }
+
+    class CreateAccountTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            final Context context = getActivity();
+
+            final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
+            String email = email_input.getText().toString();
+            String password = password_input.getText().toString();
+            ref.createUser(email, password, new Firebase.ResultHandler() {
+                @Override
+                public void onSuccess() {
+                    // Create user in database
+                    Map<String, String> newUser = new HashMap<String, String>();
+                    newUser.put("firstName", firstName_input.getText().toString());
+                    newUser.put("lastName", lastName_input.getText().toString());
+                    newUser.put("email", email_input.getText().toString());
+
+                    Firebase newRef = ref.child("/users/").push();
+                    newRef.setValue(newUser, email_input.getText().toString());
+
+                    // Go back to login page
+                    Intent intent = new Intent(getActivity(), ConnectActivity.class);
+                    startActivity(intent);
+
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    System.out.println("Error code : " + firebaseError.getCode());
+                    String message = "";
+                    switch(firebaseError.getCode()){
+                        case -18:
+                            message = context.getString(R.string.errorMessage_emailAlreadyUsed);
+                            email_input.setError( context.getString(R.string.errorMessage_emailAlreadyUsed) );
+                            email_input.setText("");
+                    }
+
+                    // Show dialog to display error
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage(message)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                }
+            });
+
+            return null;
+        }
     }
 }
