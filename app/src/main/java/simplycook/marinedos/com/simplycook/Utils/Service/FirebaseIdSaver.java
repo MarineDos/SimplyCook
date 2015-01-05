7e0a6be03b0f88318a0d9b0e6a717e93c61c299e
MarineDos <marinedos@gmail.com>
2015-01-05 17:35:36 +0100

    Push notification for new messages every 2 hours

---
+++ app/src/main/java/simplycook/marinedos/com/simplycook/Utils/Service/FirebaseIdSaver.java
@@ -0,0 +1,10 @@
+package simplycook.marinedos.com.simplycook.Utils.Service;
+
+
+public class FirebaseIdSaver {
+    public static String firebaseId;
+
+    public void saveFirebaseId(String _firebaseId){
+        firebaseId = _firebaseId;
+    }
+}
--- app/src/main/java/simplycook/marinedos/com/simplycook/Utils/Service/SuggestNotificationService.java
+++ app/src/main/java/simplycook/marinedos/com/simplycook/Utils/Service/SuggestNotificationService.java
@@ -1,11 +1,13 @@
 package simplycook.marinedos.com.simplycook.Utils.Service;
 
 
+import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
@@ -20,25 +22,33 @@ import simplycook.marinedos.com.simplycook.R;
 
 public class SuggestNotificationService extends IntentService{
 
-    private final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
-
     public SuggestNotificationService() {
         super("SuggestNotificationService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        final Context context = this;
-        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        String search;
-        final String[] firebaseId = new String[1];
+
+        // Programm next call to ths service
+        Intent serviceIntent = new Intent(this, SuggestNotificationService.class);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 60 * 1000 * 100 * 2, PendingIntent.getService(this, 0, serviceIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+        System.out.println("Refresh notification");
+
+        Firebase.setAndroidContext(this);
+        final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
+
         final AuthData authData = ref.getAuth();
 
         if (authData != null) {
+
+            System.out.println("Serach with searched id");
+            String search;
             // If user if connected with Facebook
-            if(authData.getProvider().equals("facebook")){
+            if (authData.getProvider().equals("facebook")) {
                 search = "id";
-            }else{
+            } else {
                 search = "email";
             }
 
@@ -51,72 +61,82 @@ public class SuggestNotificationService extends IntentService{
                         @Override
                         public void onDataChange(DataSnapshot snapshot) {
                             boolean exists = (snapshot.getValue() != null);
-                            if(exists) {
-                                for(DataSnapshot keys : snapshot.getChildren()){
-                                    firebaseId[0] = keys.getKey();
+                            if (exists) {
+                                for (DataSnapshot keys : snapshot.getChildren()) {
+                                    FirebaseIdSaver.firebaseId = keys.getKey();
                                 }
 
-                                // Search if there are entry in messages
-                                ref.child("users/" + firebaseId[0] + "/messages")
-                                        .addValueEventListener(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                                if (dataSnapshot != null) {
-                                                    long nbMessage = dataSnapshot.getChildrenCount();
-
-                                                    if (nbMessage > 0) {
-                                                        // There is unread messages
-                                                        String title;
-                                                        String message;
-                                                        if (nbMessage == 1) {
-                                                            title = "Vous avez un nouveau message";
-                                                            message = nbMessage + " suggestion non traitée";
-                                                        } else {
-                                                            title = "Vous avez des nouveaux messages";
-                                                            message = nbMessage + " suggestions non traitées";
-                                                        }
-
-                                                        // Construct notification
-                                                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                                                                .setSmallIcon(R.drawable.ic_launcher)
-                                                                .setContentTitle(title)
-                                                                .setContentText(message)
-                                                                .setAutoCancel(true);
-
-                                                        Intent resultIntent = new Intent(context, ConnectActivity.class);
-                                                        // Ensures that navigating backward from the Activity leads out of your application to the Home screen.
-                                                        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-                                                        // Adds the back stack for the Intent (but not the Intent itself)
-                                                        stackBuilder.addParentStack(ConnectActivity.class);
-                                                        // Adds the Intent that starts the Activity to the top of the stack
-                                                        stackBuilder.addNextIntent(resultIntent);
-                                                        PendingIntent resultPendingIntent =
-                                                                stackBuilder.getPendingIntent(
-                                                                        0,
-                                                                        PendingIntent.FLAG_UPDATE_CURRENT
-                                                                );
-
-                                                        builder.setContentIntent(resultPendingIntent);
-                                                        notificationManager.notify(12, builder.build());
-
-                                                    }
-                                                }
-                                            }
-
-                                            @Override
-                                            public void onCancelled(FirebaseError firebaseError) {
-                                            }
-                                        });
+                                searchForEntries();
                             }
                         }
 
                         @Override
-                        public void onCancelled(FirebaseError firebaseError) {}
+                        public void onCancelled(FirebaseError firebaseError) {
+                        }
                     });
 
+        } else if(FirebaseIdSaver.firebaseId != null) {
+            System.out.println("Serach with saved id");
+            searchForEntries();
         }
 
+    }
 
+    private void searchForEntries(){
+        final Context context = this;
+        final Firebase ref = new Firebase("https://simplycook.firebaseio.com");
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Search if there are entry in messages
+        ref.child("users/" + FirebaseIdSaver.firebaseId + "/messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot != null) {
+                            long nbMessage = dataSnapshot.getChildrenCount();
+
+                            if (nbMessage > 0) {
+                                // There is unread messages
+                                String title;
+                                String message;
+                                if (nbMessage == 1) {
+                                    title = "Vous avez un nouveau message";
+                                    message = nbMessage + " suggestion non traitée";
+                                } else {
+                                    title = "Vous avez des nouveaux messages";
+                                    message = nbMessage + " suggestions non traitées";
+                                }
+
+                                // Construct notification
+                                NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                                        .setSmallIcon(R.drawable.ic_launcher)
+                                        .setContentTitle(title)
+                                        .setContentText(message)
+                                        .setAutoCancel(true);
+
+                                Intent resultIntent = new Intent(context, ConnectActivity.class);
+                                // Ensures that navigating backward from the Activity leads out of your application to the Home screen.
+                                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                                // Adds the back stack for the Intent (but not the Intent itself)
+                                stackBuilder.addParentStack(ConnectActivity.class);
+                                // Adds the Intent that starts the Activity to the top of the stack
+                                stackBuilder.addNextIntent(resultIntent);
+                                PendingIntent resultPendingIntent =
+                                        stackBuilder.getPendingIntent(
+                                                0,
+                                                PendingIntent.FLAG_UPDATE_CURRENT
+                                        );
+
+                                builder.setContentIntent(resultPendingIntent);
+                                notificationManager.notify(12, builder.build());
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
+                });
     }
 }
